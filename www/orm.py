541af6f9aec6cff9678c642398b18ba4a4ace8bf
KrisYWL <1310834307@qq.com>
2018-01-01 06:41:09 -0800

    www

---
+++ www/orm.py
@@ -0,0 +1,15 @@
+
+
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+__author__='ywl'
+
+import asyncio,logging
+
+import aiomysql
+
+def log(sql,args=()):
+	logging.info('SQL: %s' %sql)
+
+async 
